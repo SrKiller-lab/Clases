2eeff12808d61eeeb771b7e3819cd9da299a546d
SrKiller-lab <pavyelyo@gmail.com>
2025-05-12 16:15:48 -0600

    clases.py

--- clases.py
+++ clases.py
@@ -1,14 +1,15 @@
 #Gomez Lopez Sergio
 class armamento:
-    def __init__ (self, daño, tipo, calidad):
+    def __init__ (self, daño, tipo, calidad): #se define la funcion 
         self.daño = daño
         self.tipo = tipo
         self.calidad = calidad
-melee = armamento("120", "cuerpo a cuerpo", "Legendario")
+melee = armamento("120", "cuerpo a cuerpo", "Legendario") #se colocan los valores de cada clase
 rango = armamento("100", "daño a distancia", "Despiadado")
 mago = armamento("70", "magia", "Roto")
 invocador= armamento("25", "invocacion", "Mitico")
 
+#se manda un print que pondra todos los atributos que se nombraron anteriormente
 print(melee.tipo)
 print(rango.daño)
 print(mago.calidad)
